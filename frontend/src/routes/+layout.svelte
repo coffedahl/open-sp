137fe95214ad1ff0f76ab998c1f8037de081c9c4
coffedahl <coffedahl@hotmail.com>
2023-02-14 23:56:38 +0100

    Added github link

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,12 @@
 	<div class="title">
 		<h1>Open-SP</h1>
 	</div>
-	<div class="deco" />
+	<div class="deco">
+		<a href="https://www.github.com/coffedahl/kjell-delim">
+			<img src="/github-mark.png" alt="Github logo" />
+			<p>Github</p>
+		</a>
+	</div>
 </header>
 
 <slot />
@@ -19,8 +24,20 @@
 		background-color: rgb(25, 117, 188);
 	}
 	.deco {
+		display: flex;
+		justify-content: end;
 		height: 3vh;
 		background-color: #f5f5f5;
+		padding-right: 10vw;
+	}
+	.deco a{
+		display: flex;
+		align-items: center;
+		color: #767676;
+		text-decoration: none;
+	}
+	.deco img{
+		height: 2.3vh;
 	}
 	h1 {
 		color: white;
