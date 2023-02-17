ae9a094295d97e8b2a3af3f0c5f6bfb135b07153
coffedahl <coffedahl@hotmail.com>
2023-02-17 15:49:02 +0100

    Created a layout for the wiki pages

--- frontend/src/routes/wiki/+page.svelte
+++ frontend/src/routes/wiki/+page.svelte
@@ -1,53 +1,34 @@
-<script>
-	import Navigation from "./navigation.svelte";
-	let nav;
-	$: page = nav;
-</script>
-<div class="main">
-	<div class="nav">
-		<Navigation bind:navURL={nav}/>
-	</div>
-	<div class="article">
-		<h1>{page}</h1>
-		<p>
-			To use the page you go to team and go to the "Prisjusteringer" channel and then you can copy
-			strait over all the new promotional prices.
-		</p>
-		<p>
-			Paste the clipboard in to the field at the top of the page. If you want to have multiple days
-			copied in at one time you can just put a space between the different days.
-		</p>
-		<p>
-			When all is pasted in to the field you can check the "Attack Clones?" box if you want the
-			program to remove duplicates from the strings. Press the submit button and the program will
-			fix and divide up the strings into multiple strings that are the correct length for the
-			"Inventeringslista-BC".
-		</p>
-		<p>
-			Each string has a copy button. Press it and paste the contents into the "artikelnummer" field
-			of the report. Set "hyllnr" to "ib*" and press the "Førhandsgranska" button to see all of the
-			new promotional prices that have a warehouse position in IB. You can keep doing this untill
-			you been trough all of the rows.
-		</p>
-	</div>
+<div class="article">
+	<h1>Title</h1>
+	<p>
+		To use the page you go to team and go to the "Prisjusteringer" channel and then you can copy
+		strait over all the new promotional prices.
+	</p>
+	<p>
+		Paste the clipboard in to the field at the top of the page. If you want to have multiple days
+		copied in at one time you can just put a space between the different days.
+	</p>
+	<p>
+		When all is pasted in to the field you can check the "Attack Clones?" box if you want the
+		program to remove duplicates from the strings. Press the submit button and the program will fix
+		and divide up the strings into multiple strings that are the correct length for the
+		"Inventeringslista-BC".
+	</p>
+	<p>
+		Each string has a copy button. Press it and paste the contents into the "artikelnummer" field of
+		the report. Set "hyllnr" to "ib*" and press the "Førhandsgranska" button to see all of the new
+		promotional prices that have a warehouse position in IB. You can keep doing this untill you been
+		trough all of the rows.
+	</p>
 </div>
 
 <style>
-	.main {
+	.article {
 		display: flex;
 		align-items: center;
-        justify-content: space-between;
-	}
-    .article{
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        width: 80vw;
+		flex-direction: column;
+		width: 80vw;
 		padding: 0 8vw;
-    }
-	.nav{
-		width: 20vw;
-		height: 100%;
 	}
 	p {
 		margin-top: 1em;
