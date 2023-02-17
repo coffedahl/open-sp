e9392604c77b977310e208312de0805a3a06339a
coffedahl <coffedahl@hotmail.com>
2023-02-17 15:49:20 +0100

    Created a dynamic route for the wiki pages

---
+++ frontend/src/routes/wiki/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let data;
+</script>
+
+<div class="main">
+	<h1>{data.title}</h1>
+</div>
+
+<style>
+    div{
+        flex-direction: column;
+        align-items: center;
+        justify-content: start;
+    }
+</style>
