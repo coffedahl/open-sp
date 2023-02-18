445709c7305041fbac804cc77ad323f9933b8575
coffedahl <coffedahl@hotmail.com>
2023-02-18 09:25:07 +0100

    Changed the wiki article to a correct one

--- frontend/src/routes/wiki/+page.svelte
+++ frontend/src/routes/wiki/+page.svelte
@@ -1,24 +1,7 @@
 <div class="article">
-	<h1>Title</h1>
-	<p>
-		To use the page you go to team and go to the "Prisjusteringer" channel and then you can copy
-		strait over all the new promotional prices.
-	</p>
-	<p>
-		Paste the clipboard in to the field at the top of the page. If you want to have multiple days
-		copied in at one time you can just put a space between the different days.
-	</p>
-	<p>
-		When all is pasted in to the field you can check the "Attack Clones?" box if you want the
-		program to remove duplicates from the strings. Press the submit button and the program will fix
-		and divide up the strings into multiple strings that are the correct length for the
-		"Inventeringslista-BC".
-	</p>
-	<p>
-		Each string has a copy button. Press it and paste the contents into the "artikelnummer" field of
-		the report. Set "hyllnr" to "ib*" and press the "Førhandsgranska" button to see all of the new
-		promotional prices that have a warehouse position in IB. You can keep doing this untill you been
-		trough all of the rows.
+	<h1>Open-SP Wiki</h1>
+	<p>This is a wiki for all the tools available on the Open-SP site.
+		Here you can find guides and information on all buttons available.
 	</p>
 </div>
 
