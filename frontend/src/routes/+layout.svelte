184fa6248b0d4140a8924f6f5375569c0c5ad52f
coffedahl <coffedahl@hotmail.com>
2023-02-19 00:43:25 +0100

    Added dropdown to navigation

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,26 @@
+<script>
+	let dropdown = false;
+</script>
+
 <header>
 	<div class="title">
 		<a href="/"><h1>Open-SP</h1></a>
 		<nav class="space-even">
-			<p class="space-even">Tools <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 1024 1024"><path d="M759.2 419.8L697.4 358 512 543.4 326.6 358l-61.8 61.8L512 667z"/></svg></p>
+			<button
+				class="space-even"
+				on:click={() => (dropdown ? (dropdown = false) : (dropdown = true))}
+			>
+				<p>Tools</p>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 1024 1024">
+					<path d="M759.2 419.8L697.4 358 512 543.4 326.6 358l-61.8 61.8L512 667z" />
+				</svg>
+			</button>
+			{#if dropdown}
+				<div class="dropdown">
+					<a href="/price">Price Adjustment</a>
+					<a href="/shipment">Shipment correction</a>
+				</div>
+			{/if}
 			<a href="/wiki">Wiki</a>
 		</nav>
 	</div>
@@ -25,7 +43,7 @@
 		display: flex;
 		justify-content: end;
 		align-items: center;
-		background-color: rgb(25, 117, 188);
+		background-color: #1975bc;
 	}
 	.deco {
 		display: flex;
@@ -34,32 +52,50 @@
 		background-color: #f5f5f5;
 		padding-right: 10vw;
 	}
-	.deco a{
+	.deco a {
 		display: flex;
 		align-items: center;
 		color: #767676;
 		text-decoration: none;
 	}
-	.deco img{
+	.deco img {
 		height: 2.3vh;
 	}
-	.space-even{
+	.space-even {
 		display: flex;
 		justify-content: space-evenly;
 		align-items: center;
 	}
-	p svg{
+	.dropdown{
+		position: absolute;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: #1975bc;
+		padding: 1em;
+		margin-top: 14vh;
+		margin-left: -4vw;
+	}
+	button {
+		background-color: transparent;
+		border: none;
+	}
+	button p {
+		font-size: medium;
+	}
+	button svg {
 		width: 1vw;
 	}
-	nav{
+	nav {
 		margin-right: 5vw;
 		width: 10vw;
 	}
-	h1{
+	h1 {
 		margin-right: 31vw;
 	}
-	nav p,a{
-		color:white;
+	nav p,
+	a {
+		color: white;
 		text-decoration: none;
 	}
 	footer {
