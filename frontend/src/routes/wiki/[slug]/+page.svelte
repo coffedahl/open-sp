b318df5cde83444135d02945db847ffe8f1a3a17
coffedahl <coffedahl@hotmail.com>
2023-02-18 10:27:53 +0100

    Created page layout for the wiki pages

--- frontend/src/routes/wiki/[slug]/+page.svelte
+++ frontend/src/routes/wiki/[slug]/+page.svelte
@@ -3,14 +3,24 @@
 </script>
 
 <div>
-    {#if data.title == "shipmentcorrection"}
-        <h1>Shipment Correction</h1>
-    {:else if data.title == "priceadjustment"}
-        <h1>Price Adjustment</h1>
-    {:else}
-        <h1>404 Page not found</h1>
-    {/if}
-
+    <div class="article">
+        {#if data.title == "shipmentcorrection"}
+            <h1>Shipment Correction</h1>
+            <h3>Description</h3>
+            <p>This tool is used for making sure all the products in a shipment has arrived successfully. The tool will help with showing the diffs from the ISR</p>
+            <h3>Guide</h3>
+            <p>Guide</p>
+        {:else if data.title == "priceadjustment"}
+            <h1>Price Adjustment</h1>
+            <h3>Description</h3>
+            <p>This tool will help to prepare the priceadjustment strings that arrives from Airtable every morning in Teams for entering in to the "Inventeringslista-BC".</p>
+            <h3>Guide</h3>
+            <p>Guide</p>
+        {:else}
+            <h1>404 Page not found</h1>
+            <p>We couldn't find the page {data.title} that you were looking for</p>
+        {/if}
+    </div>
 </div>
 
 <style>
@@ -20,5 +30,8 @@
         flex-direction: column;
         align-items: center;
         justify-content: start;
+    }
+    .article{
+        width: 80%;
     }
 </style>
