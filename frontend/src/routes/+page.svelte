0acc6a4660a98bcc1ca395527596a28031bc0a8c
coffedahl <coffedahl@hotmail.com>
2023-02-19 17:20:36 +0100

    Added shipment correction button to start screen

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <div class="main center column">
 	<h1>Start</h1>
 	<a href="/price-adjustment"><button class="center"><i class="fas fa-tag"></i><p>Price Adjustment</p></button></a>
+	<a href="/shipment-correction"><button class="center"><i class="fas fa-truck"></i><p>Shipment Correction</p></button></a>
 	<a href="/wiki"><button class="center"><i class="fas fa-book"></i><p>Wiki</p></button></a>
 </div>
 
