915913266e084d03978c225a50039580f35ae028
coffedahl <coffedahl@hotmail.com>
2023-02-19 16:58:06 +0100

    Added hover an icons to the start menu

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,18 +1,28 @@
 <div class="main center column">
 	<h1>Start</h1>
-	<a href="/price-adjustment"><button>Price Adjustment</button></a>
-	<a href="/wiki"><button>Wiki</button></a>
+	<a href="/price-adjustment"><button class="center"><i class="fas fa-tag"></i><p>Price Adjustment</p></button></a>
+	<a href="/wiki"><button class="center"><i class="fas fa-book"></i><p>Wiki</p></button></a>
 </div>
 
 <style>
+    a{
+        text-decoration: none;
+    }
+    p{
+        margin-left: 5px;
+    }
 	button {
 		width: 30vw;
-		height: 6vh;
+		height: 7vh;
 		margin-top: 3vh;
 		border-radius: 0.75em;
 		border: none;
 		background-color: #1975bc;
         color: white;
         font-size: large;
+        font-weight: 500;
 	}
+    button:hover{
+        background-color: #15639f;
+    }
 </style>
