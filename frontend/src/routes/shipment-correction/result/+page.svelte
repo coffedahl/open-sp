c2dd7030f5bcc65d8443982257c1c28a904b8602
coffedahl <coffedahl@hotmail.com>
2023-02-22 00:24:24 +0100

    Added functionality to result page and created redirect if empty

--- frontend/src/routes/shipment-correction/result/+page.svelte
+++ frontend/src/routes/shipment-correction/result/+page.svelte
@@ -1,2 +1,57 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	// Stores
+	import { shipmentData } from '../../../stores';
+	let shipment;
+	shipmentData.subscribe((value) => {
+		shipment = value;
+	});
+
+	// On mount
+	onMount(() => {
+		if (shipment.length == 0) {
+			goto('/shipment-correction');
+		}
+	});
+</script>
+
 <div class="main center column">
+	<table>
+		<tr>
+			<th>Artnr</th>
+			<th>Namn</th>
+			<th>Antal</th>
+			<th>Räknat antal</th>
+			<th>Diff</th>
+		</tr>
+		{#each shipment as product}
+			<tr>
+				<td>{product.artnr}</td>
+				<td>{product.namn}</td>
+				<td>{product.antal}</td>
+				<td>{product.actual}</td>
+				<td class={product.antal - product.actual != 0 ? 'red' : 'green'}
+					>{product.antal - product.actual}</td
+				>
+			</tr>
+		{/each}
+	</table>
 </div>
+
+<style>
+	table {
+		border-collapse: collapse;
+	}
+	th,
+	td {
+		border: 1px solid black;
+		padding: 0.3em;
+	}
+	.red {
+		background-color: red;
+	}
+	.green {
+		background-color: green;
+	}
+</style>
