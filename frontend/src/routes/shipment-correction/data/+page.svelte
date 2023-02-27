cf1685a447236a3d33a55348afbcda04cb8ef704
coffedahl <coffedahl@hotmail.com>
2023-02-28 00:41:48 +0100

    Added loader and removed undefined bug

--- frontend/src/routes/shipment-correction/data/+page.svelte
+++ frontend/src/routes/shipment-correction/data/+page.svelte
@@ -3,7 +3,8 @@
 	import { callFocus } from '../../../global';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
-	import Loader from './loader.svelte'
+	import Loader from './loader.svelte';
+
 	// Stores
 	import { shipmentData } from '../../../stores';
 	let shipment;
@@ -37,9 +38,10 @@
 	// Handle the artnr entry
 	function handleButton() {
 		// Find object where article number is and set current object
-		currentObject = shipment.find((e) => e.artnr == input);
-		if (currentObject == undefined) {
-			currentObject = '';
+		if (input != undefined && input != "") {
+			currentObject = shipment.find((e) => e.artnr == input);
+		} else {
+			alert('cannot be empty');
 		}
 		//reset input
 		input = '';
@@ -60,27 +62,53 @@
 
 <div class="main center column">
 	{#if currentObject == ''}
-	<Loader />
-	<input use:callFocus autofocus type="text" placeholder="Article number" on:keydown={handleKeydown}
-		bind:value={input} id="artnr" />
-	<button class="enter" on:click={handleButton}>Enter <i class="fas fa-right-to-bracket" /></button>
-	<button class="done" on:click={()=> goto('/shipment-correction/result')}
-		>Done <i class="fas fa-check" /></button>
-	<button class="reset" on:click={()=> {
-		shipmentData.set([]);
-		goto('/shipment-correction');
-		}}>Reset <i class="fas fa-rotate-left" /></button>
+		<div class="loader">
+			<Loader />
+		</div>
+		<input
+			use:callFocus
+			autofocus
+			type="text"
+			placeholder="Article number"
+			on:keydown={handleKeydown}
+			bind:value={input}
+			id="artnr"
+		/>
+		<button class="enter" on:click={handleButton}
+			>Enter <i class="fas fa-right-to-bracket" /></button
+		>
+		<button class="done" on:click={() => goto('/shipment-correction/result')}
+			>Done <i class="fas fa-check" /></button
+		>
+		<button
+			class="reset"
+			on:click={() => {
+				shipmentData.set([]);
+				goto('/shipment-correction');
+			}}>Reset <i class="fas fa-rotate-left" /></button
+		>
 	{:else}
-	<h1>{currentObject.namn}</h1>
-	<p>{currentObject.artnr}</p>
-	<input id="actual" use:callFocus type="number" placeholder="Antal" bind:value={actual} on:keydown={handleKeydown}
-		autocomplete="false" />
-	<button class="enter" on:click={handleEnter}>Enter <i class="fas fa-right-to-bracket" /></button>
-	<button class="clear" on:click={()=> {
-		//reset inputs anc current article
-		currentObject = '';
-		input = '';
-		}}>Clear <i class="fas fa-eraser" /></button>
+		<h1>{currentObject.namn}</h1>
+		<p>{currentObject.artnr}</p>
+		<input
+			id="actual"
+			use:callFocus
+			type="number"
+			placeholder="Antal"
+			bind:value={actual}
+			on:keydown={handleKeydown}
+			autocomplete="false"
+		/>
+		<button class="enter" on:click={handleEnter}>Enter <i class="fas fa-right-to-bracket" /></button
+		>
+		<button
+			class="clear"
+			on:click={() => {
+				//reset inputs anc current article
+				currentObject = '';
+				input = '';
+			}}>Clear <i class="fas fa-eraser" /></button
+		>
 	{/if}
 </div>
 
@@ -111,7 +139,9 @@
 		color: white;
 		font-size: medium;
 	}
-
+	.loader {
+		margin-bottom: 5vh;
+	}
 	.enter {
 		background-color: #1975bc;
 	}
--- frontend/src/routes/shipment-correction/data/loader.svelte
+++ frontend/src/routes/shipment-correction/data/loader.svelte
@@ -1,21 +1,51 @@
 <script>
-    // Import shipmentdata store
-    import { shipmentData } from '../../../stores.js'
-    let shipment;
-    shipmentData.subscribe((val) => {
-        shipment = val
-    })
+	// Import shipmentdata store
+	import { shipmentData } from '../../../stores.js';
+	let shipment;
+	shipmentData.subscribe((val) => {
+		shipment = val;
+	});
 
-    // Filter out the items that has been completed
-    let complete = shipmentData.filter((element) => {
-        if (element.actual != 0) {
-            return true;
-        } else {
-            return false
-        }
-    })
+	// Filter out the items that has been completed
+	let complete = shipment.filter((element) => {
+		if (element.actual != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	});
 </script>
 
-<div>
-    <p>{complete.length} of {shipment.data} has been completed!</p>
+<div class="container column">
+	<div class="title">
+		<h1>Progress</h1>
+		<p>{complete.length}/{shipment.length}</p>
+	</div>
+	<div class="bar rounded">
+		<div class="complete-bar rounded" style="width: {(complete.length / shipment.length) * 20}vw" />
+	</div>
 </div>
+
+<style>
+	.title{
+        display: flex;
+        justify-content: space-between;
+        align-items: end;
+		margin-bottom: 0.3em;
+    }
+    .title h1{
+        font-size: large;
+    }
+	.rounded {
+		border-radius: 1em;
+	}
+	.bar {
+		width: 20vw;
+		background-color: #cecece;
+		height: 2vh;
+	}
+	.complete-bar {
+		background-color: rgb(98, 153, 15);
+		height: 2vh;
+	}
+</style>
