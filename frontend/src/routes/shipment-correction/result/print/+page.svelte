93918167db7b36fed99c66ea10d1f280551cb6a6
coffedahl <coffedahl@hotmail.com>
2023-03-03 00:05:29 +0000

    Created input for seller id and added some information to the print report

--- frontend/src/routes/shipment-correction/result/print/+page.svelte
+++ frontend/src/routes/shipment-correction/result/print/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 
+	let seller;
 	//Stores
 	import { shipmentData } from '../../../../stores';
 	let shipment;
@@ -10,30 +11,45 @@
 	});
 	const date = new Date().toJSON().slice(0, 10);
 
-	onMount(() => {
-		window.print();
-		goto('/shipment-correction/result');
-	});
+	function handleClick() {
+		if (seller != undefined) {
+			window.print();
+			goto('/shipment-correction/result');
+		}
+	}
 </script>
 
 <div class="main column">
+	<div class="overlay center column">
+		<input type="text" placeholder="Seller ID" bind:value={seller} />
+		<button on:click={handleClick}>Print</button>
+	</div>
 	<div class="print">
-		<p>Shipment Report - {date}</p>
+		<div class="input">
+			<p>Seller: <b>{seller}</b></p>
+			<p>Shipment Report - <b>{date}</b></p>
+			<div>
+				<input type="checkbox" />
+				<p>Bokfört</p>
+			</div>
+		</div>
 		<table>
 			<tr>
 				<th>Artnr</th>
 				<th>Namn</th>
+				<th>Hyllplats</th>
 				<th>Antal</th>
 				<th>Räknat antal</th>
 				<th>Diff</th>
 			</tr>
 			{#each shipment as item}
 				<tr>
-					<td>{item.artnr}</td>
-					<td>{item.namn}</td>
-					<td>{item.antal}</td>
-					<td>{item.actual}</td>
-					<td class="count">
+					<td style="width: 8%; text-align:right;">{item.artnr}</td>
+					<td style="width: 35%;">{item.namn}</td>
+					<td style="width: 13%;">{item.hyllnr}</td>
+					<td style="width: 8%; text-align:right;">{item.antal}</td>
+					<td style="width: 8%; text-align:right;">{item.actual}</td>
+					<td style="width: 8%; text-align: center" class="count">
 						{#if item.actual - item.antal == 0}
 							<i class="fas fa-check" />
 						{:else}
@@ -50,12 +66,50 @@
 	.print {
 		display: none;
 	}
+	.overlay {
+		width: 100vw;
+		height: 80vh;
+	}
+	.overlay input {
+		width: 19vw;
+		padding: 0.5em;
+		border-radius: 0.5em;
+		border: 1px solid black;
+		margin-bottom: 0.3em;
+		font-size: medium;
+	}
+	.overlay button {
+		width: 20vw;
+		padding: 0.5em;
+		border-radius: 0.5em;
+		border: 1px solid black;
+		margin-top: 0.5em;
+		color: white;
+		font-size: medium;
+		background-color: #1975bc;
+	}
 	@media print {
+		.overlay {
+			display: none;
+		}
 		.print {
 			width: 100%;
 			display: flex;
 			flex-direction: column;
 			align-items: center;
+		}
+		.input,
+		.input div {
+			display: flex;
+		}
+		.input {
+			width: 100%;
+			justify-content: space-evenly;
+			margin-bottom: 1vh;
+		}
+		.input input {
+			margin-right: 4px;
+			margin-top: 2px;
 		}
 		table {
 			width: 100%;
@@ -73,7 +127,7 @@
 			border: 1px solid #888888;
 			background-color: #fefefe;
 			-webkit-color-adjust: exact;
-			padding: 0.1em 0.5em;
+			padding: 0.05em 0.3em;
 		}
 	}
 </style>
