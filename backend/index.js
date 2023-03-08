7c5554fe2cb85786b45d71ded2805406241c9f0f
Christoffer Dahl <coffedahl@hotmail.com>
2023-03-08 13:50:02 +0000

    Added comments and POST test function

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,32 @@
+// Import modules
 const express = require('express')
+const bodyParser = require('body-parser')
+// Create express app 
 const app = express()
+// Use modules in express app
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
+/**
+ * Default route for GET tests
+ */
 app.get("/", (req, res) => {
     res.send(JSON.stringify({ name: "Test" }))
+    console.log("Request sent")
 })
 
-app.listen(8080)
+/**
+ * Default test for POST tests
+ */
+app.post("/api/test", (req, res) => {
+    let data = req.body
+    res.send("Data recived: " + JSON.stringify(data))
+    console.log("Data recived: " + data)
+})
+
+/**
+ * Start the application
+ */
+app.listen(8080, () => {
+    console.log("Application listnening on port: 8080")
+})
