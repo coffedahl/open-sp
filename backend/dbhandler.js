3b5c03edb3deab2acd13a5e07eef14a91afa4167
Christoffer Dahl <coffedahl@hotmail.com>
2023-03-08 18:00:48 +0000

    Comments and add type for the run

--- backend/dbhandler.js
+++ backend/dbhandler.js
@@ -1,20 +1,22 @@
+// Import SurrealDB
 const Surreal = require('surrealdb.js').default;
 const db = new Surreal('http://localhost:8000/rpc');
+// Functions accesable from the outside
 module.exports = {
-    addRun: async function (storenumber) {
+    // Function for adding a run to the db
+    addRun: async function (storenumber, runtype) {
         await db.signin({
             user: "root",
             pass: "root"
         })
-
         await db.use('test', 'test')
-
         let create = await db.create("run", {
             store: storenumber,
-            time: new Date()
+            time: new Date(),
+            type: runtype
         })
-        return (create[0])
     },
+    // Function for getting all the runs from db
     getAll: async function () {
         await db.signin({
             user: "root",
@@ -24,6 +26,7 @@ module.exports = {
         let data = await db.query("SELECT * FROM run;");
         return (data[0])
     },
+    // Function to delete all the runs from db
     delete: async function () {
         await db.signin({
             user: "root",
